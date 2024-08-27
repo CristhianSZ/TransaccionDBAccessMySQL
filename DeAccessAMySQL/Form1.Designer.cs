7baa6bac6b6ba15b1f26143959449b489180f237
CristhianSZ <Cristhiansosazurita@gmail.com>
2024-08-27 19:00:48 -0300

    Primer commit

---
+++ DeAccessAMySQL/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace DeAccessAMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnMoverDatos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pBAvanceProceso = new System.Windows.Forms.ProgressBar();
+            this.tbCantidadRegistros = new System.Windows.Forms.TextBox();
+            this.dgvListadoOrigen = new System.Windows.Forms.DataGridView();
+            this.dgvListadoDestino = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoOrigen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoDestino)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMoverDatos
+            // 
+            this.btnMoverDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoverDatos.Image = ((System.Drawing.Image)(resources.GetObject("btnMoverDatos.Image")));
+            this.btnMoverDatos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMoverDatos.Location = new System.Drawing.Point(78, 212);
+            this.btnMoverDatos.Name = "btnMoverDatos";
+            this.btnMoverDatos.Size = new System.Drawing.Size(236, 61);
+            this.btnMoverDatos.TabIndex = 0;
+            this.btnMoverDatos.Text = "Mover Datos";
+            this.btnMoverDatos.UseVisualStyleBackColor = true;
+            this.btnMoverDatos.Click += new System.EventHandler(this.btnMoverDatos_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(92, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(701, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TRANSFERENCIA DE DATOS ENTRE MOTORES DE BASE DE DATOS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ORIGEN (ACCESS)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(403, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Avance del proceso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(654, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Cantidad de Registros";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "DESTINO (MYSQL)";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pBAvanceProceso
+            // 
+            this.pBAvanceProceso.Location = new System.Drawing.Point(382, 232);
+            this.pBAvanceProceso.Name = "pBAvanceProceso";
+            this.pBAvanceProceso.Size = new System.Drawing.Size(170, 32);
+            this.pBAvanceProceso.TabIndex = 6;
+            // 
+            // tbCantidadRegistros
+            // 
+            this.tbCantidadRegistros.Location = new System.Drawing.Point(695, 235);
+            this.tbCantidadRegistros.Name = "tbCantidadRegistros";
+            this.tbCantidadRegistros.Size = new System.Drawing.Size(50, 20);
+            this.tbCantidadRegistros.TabIndex = 7;
+            // 
+            // dgvListadoOrigen
+            // 
+            this.dgvListadoOrigen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoOrigen.Location = new System.Drawing.Point(37, 56);
+            this.dgvListadoOrigen.Name = "dgvListadoOrigen";
+            this.dgvListadoOrigen.Size = new System.Drawing.Size(805, 150);
+            this.dgvListadoOrigen.TabIndex = 8;
+            // 
+            // dgvListadoDestino
+            // 
+            this.dgvListadoDestino.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoDestino.Location = new System.Drawing.Point(37, 304);
+            this.dgvListadoDestino.Name = "dgvListadoDestino";
+            this.dgvListadoDestino.Size = new System.Drawing.Size(805, 150);
+            this.dgvListadoDestino.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 466);
+            this.Controls.Add(this.dgvListadoDestino);
+            this.Controls.Add(this.dgvListadoOrigen);
+            this.Controls.Add(this.tbCantidadRegistros);
+            this.Controls.Add(this.pBAvanceProceso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMoverDatos);
+            this.Name = "Form1";
+            this.Text = "Transferencias de datos ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoOrigen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoDestino)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMoverDatos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar pBAvanceProceso;
+        private System.Windows.Forms.TextBox tbCantidadRegistros;
+        private System.Windows.Forms.DataGridView dgvListadoOrigen;
+        private System.Windows.Forms.DataGridView dgvListadoDestino;
+    }
+}
+
---
+++ DeAccessAMySQL/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.Data.OleDb;
+
+
+using MySql.Data.MySqlClient;
+
+
+
+
+namespace DeAccessAMySQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDatosEnDataGridViewOrigen();
+        }
+        //Cargado de datos en la primer gridview
+        private void CargarDatosEnDataGridViewOrigen()
+        {
+            string cadenaOrigen = ConfigurationManager.ConnectionStrings["Cadena_Access_Origen"].ConnectionString;
+
+            using (OleDbConnection cnnOrigen = new OleDbConnection(cadenaOrigen))
+            {
+                try
+                {
+                    // Abrir la conexión
+                    cnnOrigen.Open();
+
+                    // Leer los datos de la tabla Productos
+                    string leerDatosSQL = "SELECT * FROM Productos";
+                    DataTable datosOrigen = new DataTable();
+
+                    using (OleDbDataAdapter dap = new OleDbDataAdapter(leerDatosSQL, cnnOrigen))
+                    {
+                        dap.Fill(datosOrigen); // Llenar el DataTable con los datos obtenidos
+                    }
+
+                    // Asignar los datos al DataGridView para mostrarlos
+                    dgvListadoOrigen.DataSource = datosOrigen;
+                }
+                catch (Exception ex)
+                {
+                    // Mostrar el error en un MessageBox
+                    MessageBox.Show("Error al cargar los datos: " + ex.Message);
+                }
+                finally
+                {
+                    //cierro la concexion
+                    if (cnnOrigen.State == ConnectionState.Open)
+                    {
+                        cnnOrigen.Close();
+                    }
+                }
+            }
+        }
+
+
+
+        // Variable para verificar si los datos ya han sido transaccionados
+        private bool datosTransaccionados = false;
+
+        //Modularizacion de btnMoverDatos
+        private void btnMoverDatos_Click(object sender, EventArgs e)
+        {
+            if (datosTransaccionados)
+            {
+                MessageBox.Show("Los datos ya han sido transaccionados.");
+                return;
+            }
+
+            btnMoverDatos.Enabled = false;
+
+            string connectionStringMySQL = "Server=localhost;Database=bddestino;Uid=root;Pwd=123456a$;";
+            string connectionStringAccess = ConfigurationManager.ConnectionStrings["Cadena_Access_Origen"].ConnectionString;
+
+            using (OleDbConnection cnnOrigen = new OleDbConnection(connectionStringAccess))
+            using (MySqlConnection cnnDestino = new MySqlConnection(connectionStringMySQL))
+            {
+                try
+                {
+                    cnnOrigen.Open();
+                    cnnDestino.Open();
+
+                    // Crear la tabla si no existe
+                    CrearTablaProductos(cnnDestino);
+
+                    // Leer datos de Access
+                    DataTable datosOrigen = LeerDatosDesdeAccess(cnnOrigen);
+
+                    // Configurar ProgressBar y TextBox
+                    ConfigurarInterfazUsuario(datosOrigen.Rows.Count);
+
+                    // Insertar datos en MySQL
+                    InsertarDatosEnMySQL(datosOrigen, cnnDestino);
+
+                    MessageBox.Show("Datos transferidos correctamente.");
+                    CargarDatosEnDataGridViewDestino();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error durante la transferencia de datos: " + ex.Message);
+                }
+                finally
+                {
+                    if (cnnOrigen.State == ConnectionState.Open) cnnOrigen.Close();
+                    if (cnnDestino.State == ConnectionState.Open) cnnDestino.Close();
+                }
+            }
+        }
+
+        private void CrearTablaProductos(MySqlConnection cnnDestino)
+        {
+            string crearTablaSQL = @"
+    CREATE TABLE IF NOT EXISTS Productos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        marca VARCHAR(50),
+        nombre VARCHAR(100),
+        descripcion VARCHAR(255),
+        categoria VARCHAR(50),
+        stock INT,
+        precioCosto DOUBLE,
+        precioVenta DOUBLE
+    )";
+
+            using (MySqlCommand cmd = new MySqlCommand(crearTablaSQL, cnnDestino))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private DataTable LeerDatosDesdeAccess(OleDbConnection cnnOrigen)
+        {
+            string leerDatosSQL = "SELECT * FROM Productos";
+            DataTable datosOrigen = new DataTable();
+
+            using (OleDbDataAdapter dap = new OleDbDataAdapter(leerDatosSQL, cnnOrigen))
+            {
+                dap.Fill(datosOrigen);
+            }
+
+            return datosOrigen;
+        }
+
+        private void ConfigurarInterfazUsuario(int cantidadRegistros)
+        {
+            pBAvanceProceso.Minimum = 0;
+            pBAvanceProceso.Maximum = cantidadRegistros;
+            pBAvanceProceso.Value = 0;
+            tbCantidadRegistros.Text = "0";
+        }
+
+        private void InsertarDatosEnMySQL(DataTable datosOrigen, MySqlConnection cnnDestino)
+        {
+            string insertarDatosSQL = @"
+    INSERT INTO Productos (marca, nombre, descripcion, categoria, stock, precioCosto, precioVenta) 
+    VALUES (@marca, @nombre, @descripcion, @categoria, @stock, @precioCosto, @precioVenta)";
+
+            using (MySqlTransaction transaccion = cnnDestino.BeginTransaction())
+            {
+                try
+                {
+                    foreach (DataRow fila in datosOrigen.Rows)
+                    {
+                        using (MySqlCommand cmd = new MySqlCommand(insertarDatosSQL, cnnDestino, transaccion))
+                        {
+                            cmd.Parameters.AddWithValue("@marca", fila["marca"]);
+                            cmd.Parameters.AddWithValue("@nombre", fila["nombre"]);
+                            cmd.Parameters.AddWithValue("@descripcion", fila["descripcion"]);
+                            cmd.Parameters.AddWithValue("@categoria", fila["categoria"]);
+                            cmd.Parameters.AddWithValue("@stock", fila["stock"]);
+                            cmd.Parameters.AddWithValue("@precioCosto", fila["precioCosto"]);
+                            cmd.Parameters.AddWithValue("@precioVenta", fila["precioVenta"]);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        pBAvanceProceso.Value++;
+                        tbCantidadRegistros.Text = pBAvanceProceso.Value.ToString();
+                        Application.DoEvents();
+                    }
+
+                    transaccion.Commit();
+                }
+                catch
+                {
+                    transaccion.Rollback();
+                    throw;
+                }
+            }
+        }
+
+
+        //Cargado de datos en el segundo gridview
+        private void CargarDatosEnDataGridViewDestino()
+        {
+            string cadenaDestino = ConfigurationManager.ConnectionStrings["Cadena_MySQL_Destino"].ConnectionString;
+
+            using (MySqlConnection cnnDestino = new MySqlConnection(cadenaDestino))
+            {
+                try
+                {
+                    // Abrir la conexión
+                    cnnDestino.Open();
+
+                    // Leer los datos de la tabla Productos
+                    string leerDatosSQL = "CALL getProductos ()";
+                    DataTable datosDestino = new DataTable();
+
+                    using (MySqlDataAdapter dap = new MySqlDataAdapter(leerDatosSQL, cnnDestino))
+                    {
+                        dap.Fill(datosDestino); // Llenar el DataTable con los datos obtenidos
+                    }
+
+                    // Asignar los datos al DataGridView para mostrarlos
+                    dgvListadoDestino.DataSource = datosDestino;
+                }
+                catch (Exception ex)
+                {
+                    // Mostrar el error en un MessageBox
+                    MessageBox.Show("Error al cargar los datos: " + ex.Message);
+                }
+            }
+        }
+
+
+
+
+              
+    }
+}
